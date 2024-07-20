189855dd2257f49d2a253039e069960b2a19834c
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2024-07-20 09:36:28 +0300

    add OG data

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,24 @@
 import 'focus-visible'
 import '@/styles/tailwind.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta
+          property="og:title"
+          content="Moët Lashes – Perfect Brows, Flawless Lashes"
+        />
+        <meta
+          property="og:description"
+          content="Transform your look with a lash and brow treatment at Moët Lashes – Cairns City."
+        />
+        <meta property="og:image" content="/images/hero-call-to-action.webp" />
+        <meta property="og:url" content="https://moet-lashes.com/" />
+        <meta property="og:type" content="website" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
